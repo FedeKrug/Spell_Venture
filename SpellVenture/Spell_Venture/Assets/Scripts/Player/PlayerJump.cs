f92a0366ed2cc6626005336a0006d83cd942adc0
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-03-30 20:07:11 -0300

    PlayerMovement

---
+++ SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+
+namespace Game.Player
+{
+	public class PlayerJump : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody2D _rb2d;
+		[SerializeField] private float _jumpForce;
+		[SerializeField, Tooltip("Jump Button name")] private string _jumpButton;
+		private void Update()
+		{
+			if (Input.GetButtonDown(_jumpButton))
+			{
+				Jump();
+			}
+
+		}
+
+		private void Jump()
+		{
+			_rb2d.velocity = new Vector2(_rb2d.velocity.x, _jumpForce);
+		}
+	}
+}
--- SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerMovement.cs
+++ SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+namespace Game.Player
+{
+	public class PlayerMovement : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody2D _rb2d;
+		[SerializeField] private float _minMovementSpeed, _maxMovementSpeed;
+		[SerializeField] private string _horizontalAxis;
+		//[SerializeField] private SpriteRenderer _spriteR;
+		private Vector2 _moveInput;
+		private float _movementSpeed;
+
+		private void Awake()
+		{
+			_movementSpeed = _minMovementSpeed;
+		}
+		private void Update()
+		{
+			_moveInput = new Vector2(Input.GetAxisRaw(_horizontalAxis), transform.position.y).normalized;
+
+			if (_moveInput.x>0)
+			{
+				transform.eulerAngles = new Vector3(0,0,0);
+			}
+			else if (_moveInput.x<0)
+			{
+				transform.eulerAngles = new Vector3(0,180,0);
+
+			}
+
+				if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+				{
+					_movementSpeed = _maxMovementSpeed;
+				}
+				else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+				{
+					_movementSpeed = _minMovementSpeed;
+				}
+		}
+		private void FixedUpdate()
+		{
+			_rb2d.velocity += _moveInput * _movementSpeed * Time.fixedDeltaTime;
+		}
+	}
 }
