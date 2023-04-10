b76abacbc6eb6b91dc8f77b391b3d57a129ac500
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-04-10 19:41:40 -0300

    PlayerInteractor

--- SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerInteractor.cs
+++ SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerInteractor.cs
@@ -1,6 +1,8 @@
 ﻿
 using Game.Interfaces;
 
+using System.Collections.Generic;
+
 using UnityEngine;
 
 
@@ -9,21 +11,26 @@ namespace Game.Player
 	public class PlayerInteractor : MonoBehaviour
 	{
 		[SerializeField] private KeyCode _interactionKey;
-		[SerializeField] private bool _canInteract;
+		[SerializeField] private List<Interactable> _interactableList = new List<Interactable>();
 
 		private void Update()
 		{
-			
+			if (_interactableList.Count <= 0) return;
+
+			if (Input.GetKeyDown(_interactionKey))
+			{
+				_interactableList[_interactableList.Count - 1]?.Interact();
+			}
+
 		}
+
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
 			if (collision.GetComponent<Interactable>() != null)
 			{
-				//_canInteract = true;
-				//if (Input.GetKeyDown(_interactionKey))
-				//{
-					collision.GetComponent<Interactable>().Interact();
-				//}
+				var interactable = collision.GetComponent<Interactable>();
+				if (interactable == null) return;
+				_interactableList.Add(interactable);
 			}
 		}
 
@@ -31,8 +38,9 @@ namespace Game.Player
 		{
 			if (collision.GetComponent<Interactable>() != null)
 			{
-				_canInteract = false;
-			
+				var interactable = collision.GetComponent<Interactable>();
+				if (interactable == null) return;
+				_interactableList.Remove(interactable);
 			}
 		}
 	}
---
+++ SpellVenture/Spell_Venture/Assets/Scripts/Player/PlayerRaycastJump.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+
+namespace Game.Player
+{
+	public class PlayerRaycastJump : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody2D _rb2d;
+		[SerializeField, Range(0,10)] private float _raycastDistance;
+		[SerializeField] private KeyCode _jumpKey;
+		[SerializeField] private bool _isGrounded;
+		[SerializeField] private float _jumpForce;
+		[SerializeField] private float _lowJumpMultiplier;
+		[SerializeField] private float _fallMultiplier;
+
+		private void Update()
+		{
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, _raycastDistance);
+			Debug.DrawRay(transform.position, new Vector3(_raycastDistance,0,0)  , Color.red);
+			if (hit.collider != null)
+			{
+				_isGrounded = true;
+			}
+			else
+			{
+				_isGrounded = false;
+			}
+			
+			if (_isGrounded && Input.GetKey(_jumpKey))
+			{
+				Jump();
+			}
+
+			if (_rb2d.velocity.y < 0) //esta cayendo
+			{
+				_rb2d.velocity += Vector2.up * Physics2D.gravity.y * (_fallMultiplier - 1) * Time.deltaTime;
+
+			}
+			else if (_rb2d.velocity.y > 0 && !Input.GetKey(_jumpKey) || (Input.GetKey(_jumpKey) && !_isGrounded)) 
+			{
+				_rb2d.velocity += Vector2.up * Physics2D.gravity.y * (_lowJumpMultiplier - 1) * Time.deltaTime;
+			}
+		}
+
+		public void Jump()
+		{
+			_rb2d.velocity = new Vector2(_rb2d.velocity.x, _jumpForce);
+		}
+
+	}
+}
